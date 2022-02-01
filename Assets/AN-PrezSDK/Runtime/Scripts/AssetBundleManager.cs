50df565f73db4f6035eeeebfa7208183027faa8b
Vinod Akula <vinodakula.coder@gmail.com>
2022-02-01 22:59:44 +0530

    Created ANPrezSDK dll and some folders organized

---
+++ Assets/AN-PrezSDK/Runtime/Scripts/AssetBundleManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AssetBundleManager : MonoBehaviour
+{
+    public static AssetBundleManager Instance
+    {
+        get
+        {
+
+            if (_instance == null)
+            {
+                _instance = CoroutineRunner.Instance.gameObject.AddComponent<AssetBundleManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    private static AssetBundleManager _instance;
+
+    private static Dictionary<string, AssetBundleContainer> _bundles = new Dictionary<string, AssetBundleContainer>();
+
+    public static void Cleanup()
+    {
+        foreach (var item in _bundles)
+        {
+            if (item.Value.Bundle)
+            {
+                item.Value.Bundle.Unload(true);
+            }
+        }
+
+        _bundles.Clear();
+    }
+
+    public static IEnumerator LoadAssetBundle(string path, Action<GameObject> OnLoaded)
+    {
+        if (_bundles.TryGetValue(path, out AssetBundleContainer bundle))
+        {
+            OnLoaded?.Invoke(bundle.GameObject);
+        }
+        else
+        {
+            string localPath = UriBuilderExtension.UriPath(path);
+            UnityWebRequest unityWebRequest = UnityWebRequestAssetBundle.GetAssetBundle(localPath);
+            yield return unityWebRequest.SendWebRequest();
+            if (string.IsNullOrEmpty(unityWebRequest.error))
+            {
+                if (unityWebRequest.isDone)
+                {
+                    var assetBundle = DownloadHandlerAssetBundle.GetContent(unityWebRequest);
+                    var assetObject = (GameObject)assetBundle.LoadAsset(assetBundle.GetAllAssetNames()[0]);
+                    var container = new AssetBundleContainer(assetBundle, Instantiate(assetObject));
+                    _bundles.Add(path, container);
+                    OnLoaded?.Invoke(container.GameObject);
+                }
+            }
+            else
+            {
+                Debug.LogError("Assetbundleerror : " + unityWebRequest.error);
+                OnLoaded?.Invoke(null);
+            }
+        }
+    }
+
+    private struct AssetBundleContainer
+    {
+        public AssetBundle Bundle;
+        public GameObject GameObject;
+
+        public AssetBundleContainer(AssetBundle bundle, GameObject go)
+        {
+            Bundle = bundle;
+            GameObject = go;
+        }
+    }
+}
+
+
+
---
+++ Assets/AN-PrezSDK/Runtime/Scripts/PrezStates.cs
@@ -0,0 +1,23 @@
+﻿
+namespace AfterNow.PrezSDK.Internal.Views
+{
+    public static class PrezStates
+    {
+        public static Presentation Presentation;
+
+        /// <summary>
+        /// From 0 to SlideCount - 1
+        /// </summary>
+        public static int CurrentSlide;
+        public static int PresentationID;
+        public static int PresenterID;
+
+        public static void Reset()
+        {
+            Presentation = null;
+            CurrentSlide = 0;
+            PresentationID = 0;
+            PresenterID = 0;
+        }
+    }
+}
